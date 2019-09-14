35742ef22c672b76d93ca5ff40bf88b02489dd28
Mehmet Baykar <mehmetbaykar2015@gmail.com>
2019-09-14 16:12:56 +0200

    Initial Commit

---
+++ MyInstag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyInstag
+//
+//  Created by Mehmet Baykar on 14/09/2019.
+//  Copyright © 2019 Mehmet Baykar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
